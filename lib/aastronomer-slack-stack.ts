660e13729c24b9ccfbb71e5588ea9f7a8255a9f7
Hadrian Hughes <hadrian.hughes@and.digital>
2020-12-17 13:45:46 +0000

    Instantiate a new HttpApi

--- lib/aastronomer-slack-stack.ts
+++ lib/aastronomer-slack-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import * as apig from '@aws-cdk/aws-apigatewayv2'
 
-export class AastronomerSlackStack extends cdk.Stack {
+export class AAStronomerSlackStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const api = new apig.HttpApi(this, 'AAStronomerSlackAPI', {
+      apiName: 'aastronomer-slack-api',
+      createDefaultStage: true
+    })
   }
 }
