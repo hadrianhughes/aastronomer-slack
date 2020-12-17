8191cd9df4d945d9dd5d117f587d731423b6dcf5
Hadrian Hughes <hadrian.hughes@and.digital>
2020-12-17 15:37:26 +0000

    Pass AWS region from .env file into CDK Stack

--- bin/aastronomer-slack.ts
+++ bin/aastronomer-slack.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
+require('dotenv').config()
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { AAStronomerSlackStack } from '../lib/aastronomer-slack-stack';
 
 const app = new cdk.App();
-new AAStronomerSlackStack(app, 'AAStronomerSlackStack');
+new AAStronomerSlackStack(app, 'AAStronomerSlackStack', {
+  env: {
+    region: process.env.REGION
+  }
+});
