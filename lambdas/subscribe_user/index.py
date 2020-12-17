9dc9a7fb9715ca2d7d34e527b9d0c9a0de4299c9
Hadrian Hughes <hadrian.hughes@and.digital>
2020-12-17 17:21:12 +0000

    Remove print statement from Lambda function

--- lambdas/subscribe_user/index.py
+++ lambdas/subscribe_user/index.py
@@ -5,7 +5,6 @@ import base64
 def handler(event: dict, context: dict) -> dict:
     body_base64 = event.get('body', '')
     body_formdata_bytes = base64.b64decode(body_base64)
-    print(body_formdata_bytes)
     body_formdata = body_formdata_bytes.decode('utf-8')
     body = parse_qs(body_formdata)
     text = body.get('text')
